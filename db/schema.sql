1a59072df555c59d8881ba3a2c797845b3b3845e
Andrew Bratcher <37027181+tanksareforcowards@users.noreply.github.com>
2020-05-26 02:32:06 -0400

    setup via readme + simple css

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
-init
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INTEGER NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY(id)
+)
--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,3 @@
-init
+INSERT INTO burgers (burger_name, devoured) VALUES ("Boneless Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Battletoads Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Burger, hold the burger", false);
